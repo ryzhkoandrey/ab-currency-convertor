61a1273f39e156530f43cf973a6292e144eead79
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-07-02 16:44:08 +0300

    +

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import { Block } from './Block';
 import './index.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
-      <Block value={0} currency="USD" />
-    </div>
-  );
+   return (
+      <div className="App">
+         <Block
+            value={0}
+            currency="RUB"
+            onChangeCurrency={(cur) => console.log(cur)}
+         />
+         <Block value={0} currency="USD" />
+      </div>
+   );
 }
 
 export default App;
