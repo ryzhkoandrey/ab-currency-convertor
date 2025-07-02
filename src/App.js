e3000e4002141c92c696755669ab7edbb2979ec2
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-07-02 19:24:19 +0300

    +

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Block } from './Block';
 import './index.scss';
 
 function App() {
+   const [fromCurrency, setFromCurrency] = React.useState('RUB');
    const [rates, setRates] = React.useState({});
 
    React.useEffect(() => {
@@ -23,10 +24,10 @@ function App() {
       <div className="App">
          <Block
             value={0}
-            currency="RUB"
+            currency="USD"
             onChangeCurrency={(cur) => console.log(cur)}
          />
-         <Block value={0} currency="USD" />
+         <Block value={0} currency="EUR" />
       </div>
    );
 }
